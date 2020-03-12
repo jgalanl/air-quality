c8ab1d1627ea1bb2855c4e7b82ac2dd13a4ae4cc
jegalanl <jegalanl@pa.uc3m.es>
2020-03-12 01:41:25 +0100

    Escritura en base de datos

---
+++ modules/db/db.py
@@ -0,0 +1,23 @@
+from firebase import firebase
+from datetime import date
+
+def insert(temperature, pressure, humidity, gas_resistance):
+    try:
+        firebase_api = firebase.FirebaseApplication('https://air-quality-2ab4d.firebaseio.com/', None)
+
+        data = {
+            "Date": {
+                "day": date.today().strftime("%H:%M:%S, %d/%m/%Y")
+            },
+            "Data": {
+                "temperature": temperature,
+                "pressure": pressure,
+                "humidity": humidity,
+                "gas_resistance": gas_resistance
+            }
+        }
+        
+        firebase_api.post('/Raspberry/00000000a44b23f9', data)
+
+    except Exception as exc:
+        print(exc)
