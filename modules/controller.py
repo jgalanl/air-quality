7351ac56cb9ee1c1ed20c03dac3d22ba507ed5ff
jegalanl <jegalanl@pa.uc3m.es>
2020-05-23 12:38:30 +0200

    Cambios process

--- modules/controller.py
+++
@@ -1,6 +0,0 @@
-import os
-import time
-
-while True:
-    os.system('python modules\process.py')
-    time.sleep(3600)
--- modules/process.py
+++ modules/process.py
@@ -1,18 +1,22 @@
-import json
+import time
 from sensor import read_data
 from api import get_api
 
+
 if __name__ == "__main__":
-    print('\n--- INICIO DE EJECUCCIÓN ---')
 
-    # 1º llamamos a la Weather API
-    get_api()
+    while True:
+        print('\n--- INICIO DE EJECUCCIÓN ---')
+        # 1º llamamos a la Weather API
+        get_api()
 
-    # Llamar al read_data del sensor
-    read_data()
+        # Llamar al read_data del sensor
+        read_data()
+        
+        time.sleep(3600)
+
+   
 
     # TODO Llamar a la clase predictive para realizar la predicción del modelo
 
-    #while True:
-    #os.system('python3 sensor.py')
-    #time.sleep(3600)
+    
